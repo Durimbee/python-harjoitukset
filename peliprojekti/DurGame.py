303b7ae94511c944ccf9665a94848318204bf6f1
Durim Beqirii <durim.2003@icloud.com>
2026-09-03 00:07:25 +0300

    Projekti.2

--- peliprojekti/DurGame.py
+++ peliprojekti/DurGame.py
@@ -6,5 +6,29 @@ ikä = int(input("Mikä on ikäsi?"))
 print("Pelaajan nimi: ", nimi)
 print("Pelaajan ikä: ", ikä)
 
+#Ikäraja
+if ikä <12:
+    print("Olet liian nuori pelaamaan.")
+else:
+    print("Tervetuloa pelaamaan", nimi + "!")
+    print(" PÄÄVALIKKO ")
+    print("1) Aloita peli")
+    print("2) Ohjeet")
+    print("3) Lopeta peli")
 
-                                                                
+while True:
+    komento = input("Anna komento: ")
+    if komento == "Lopeta peli":
+        print("Peli keskeytetään. Kiitos pelaamisesta!")
+        break
+    elif komento == "Aloita peli":
+        print("Aloitetaan peliä...")
+    elif komento == "Ohjeet":
+        print("Tässä ohjeet...")
+    else:
+        print("Saatavilla olevat komennot: Aloita peli, Ohjeet, Lopeta peli")
+
+print(" PÄÄVALIKKO ")
+print("1) Aloita peli")
+print("2) Ohjeet")
+print("3) Lopeta")
