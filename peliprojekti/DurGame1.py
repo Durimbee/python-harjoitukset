8a81c0cce3eabafac7ca039c1deeb5270b87572d
Durim Beqirii <durim.2003@icloud.com>
2026-09-03 00:09:58 +0300

    Durgame

---
+++ peliprojekti/DurGame1.py
@@ -0,0 +1,7 @@
+#Kysytään pelaajan nimi ja ikä
+nimi = input("Mikä on nimesi?")
+ikä = int(input("Mikä on ikäsi?"))
+
+#Tulostetaan pelaajan nimi ja ikä
+print("Pelaajan nimi: ", nimi)
+print("Pelaajan ikä: ", ikä)
