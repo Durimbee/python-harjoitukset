3b2a78730a3dfa43a8a89c0f8dda6a37ad53eea3
Durim Beqirii <durim.2003@icloud.com>
2026-09-11 13:52:10 +0300

    11.09.2026

--- mod04/m4t4.py
+++ mod04/m4t4.py
@@ -8,3 +8,12 @@ elif vuosi % 4 == 0:
     print(vuosi, "on karkausvuosi")
 else:
     print(vuosi, "ei ole karkausvuosi")
+
+
+vuosi = int(input("Anna vuosiluku: "))
+if vuosi % 4 == 0 and vuosi % 100 != 0:
+    print("Vuosi on karkausvuosi")
+elif vuosi % 100 == 0 and vuosi % 400 == 0:
+    print("Vuosi on karkausvuosi")
+else:
+    print("Vuosi ei ole karkausvuosi")
--- mod05/m5t1.py
+++ mod05/m5t1.py
@@ -1,5 +1,5 @@
-luku = 1
+luku = 3
+
 while luku <= 1000:
-    if luku % 3 == 0:
-        print(luku)
-    luku = luku +1
+    print(luku)
+    luku = luku + 3
